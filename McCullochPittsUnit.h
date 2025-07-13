b5f928848f16b6db228ccb0c70b226b92e153b38
Rhythm Dutta <duttarhythm18@gmail.com>
2025-07-13 15:33:05 +0530

    Necessary files added

---
+++ McCullochPittsUnit.h
@@ -0,0 +1,15 @@
+#pragma once
+class McCullochPittsUnit
+{
+	int m_x1, m_x2;
+
+public:
+	McCullochPittsUnit(int x1, int x2) : m_x1(x1), m_x2(x2) {}
+
+	int g()
+	{
+		return m_x1 + m_x2;
+	}
+
+
+};
